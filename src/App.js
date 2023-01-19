c310519a2cd36c2791a323630f497e42c90b5e96
Mark Mauro <markymauro@gmail.com>
2023-01-19 18:51:55 -0500

    merge was a success

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import NavBar from "./NavBar";
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
       <NavBar />
-=======
-    <>    
-    <div className="App">
-      <h1>newgradnomad.com</h1>
-    </div>
->>>>>>> b41a582615d2e29550dd64048e624c487194edbb
     </>
   );
 }
