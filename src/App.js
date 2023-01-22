ef1bfc9a3f2a322ec636d51ecadcbd379aecd17b
Mark Mauro <markymauro@gmail.com>
2023-01-22 12:10:38 -0500

    organzing components and styles, styling herosection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import NavBar from "./NavBar";
-import HeroSection from "./HeroSection";
+import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
