4b40a62c6eebfda595fd2c89530b5d85c4cb188b
Kenneth Doerflein <ktd500@verizon.net>
2023-01-28 23:48:53 -0500

    update button color to orange

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -6,9 +6,9 @@ export default function NotFound() {
     <Container className="text-center">
       <h1 className="mt-3">Oops! You seem to be lost.</h1>
       <p>Here are some helpful links:</p>
-      <Button variant="dark" className="mx-2" href="/">Home</Button>
-      <Button variant="dark" className="mx-2" href="/PostAJob">Post a Job</Button>
-      <Button variant="dark" className="mx-2" href="/about">About</Button>
+      <Button style={{'background-color':"#ed6f26", border:"none"}} className="mx-2" href="/">Home</Button>
+      <Button style={{'background-color':"#ed6f26", border:"none"}} className="mx-2" href="/PostAJob">Post a Job</Button>
+      <Button style={{'background-color':"#ed6f26", border:"none"}} className="mx-2" href="/about">About</Button>
     </Container>
   );
 }
