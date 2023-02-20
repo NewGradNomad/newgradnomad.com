07b15ed09fba144776d397722fb0f80d5c55d201
Mark Mauro <markymauro@gmail.com>
2023-02-20 13:16:54 -0500

    changing job posting card name

--- src/components/JobPosting/JobPostings.js
+++ src/components/JobPosting/JobPostings.js
@@ -3,16 +3,16 @@ import { Container, Card, Button } from "react-bootstrap";
 import React from 'react'
 import './JobPostings.css'
 import { useEffect, useState } from "react";
-import ToggleCard from "./ToggleCard";
+import StandardCard from "./StandardCard";
 
 export default class JobSearch extends Component {
     render(){
         return (
             <>
-                {/* this page will be used to render job postings, ToggleCard.js is where the "Job Posting" lives per se */}
-                <ToggleCard/>
-                <ToggleCard/>
-                <ToggleCard/>
+                {/* this page will be used to render job postings, StandardCard.js is where the "Job Posting" lives per se */}
+                <StandardCard/>
+                <StandardCard/>
+                <StandardCard/>
             </>
         );
     }
