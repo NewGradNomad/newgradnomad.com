68d030a947cc24ee27d9c355f3e87a912e5ddde8
Mark Mauro <markymauro@gmail.com>
2023-02-20 13:42:46 -0500

    creation of pinned job posting

--- src/components/JobPosting/JobPostings.js
+++ src/components/JobPosting/JobPostings.js
@@ -3,16 +3,17 @@ import { Container, Card, Button } from "react-bootstrap";
 import React from 'react'
 import './JobPostings.css'
 import { useEffect, useState } from "react";
-import StandardCard from "./StandardCard";
+import StandardPosting from "./StandardPosting";
+import PinnedPosting from "./PinnedPosting";
 
 export default class JobSearch extends Component {
     render(){
         return (
             <>
-                {/* this page will be used to render job postings, StandardCard.js is where the "Job Posting" lives per se */}
-                <StandardCard/>
-                <StandardCard/>
-                <StandardCard/>
+                {/* this page will be used to render job postings, StandardPosting.js is where the "Job Posting" lives per se */}
+                <PinnedPosting/>
+                <StandardPosting/>
+                <StandardPosting/>
             </>
         );
     }
